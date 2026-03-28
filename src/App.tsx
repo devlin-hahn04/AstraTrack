89bd2f38faa6f408e705aa9cd539a584cf4e98f4
devlin-hahn04 <devlin.hahn@upr.edu>
2026-03-27 21:33:41 -0400

    Added in new globe plugin with prisms of coordinates for major airports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import GlobeView from "../src/globe/globe_view.tsx";
+import GlobeView from "./globe/globe_view";
 
 function App() {
-  return <GlobeView />;
+  return (
+    <div style={{ 
+      width: "100vw", 
+      height: "100vh", 
+      margin: 0,
+      padding: 0,
+      position: "fixed",
+      top: 0,
+      left: 0,
+      overflow: "hidden"
+    }}>
+      <GlobeView />
+    </div>
+  );
 }
 
 export default App;
--- src/globe/globe_view.tsx
+++ src/globe/globe_view.tsx
@@ -1,7 +1,7 @@
+// globe_view.tsx
 import { useEffect, useRef } from "react";
-import Globe from "globe.gl";
-import * as topojson from "topojson-client";
-import * as turf from "@turf/turf";
+import { BarGlob3d } from "glob3d";
+import { airportsData } from "./airport_data";
 
 function GlobeView() {
   const globeRef = useRef<HTMLDivElement | null>(null);
@@ -9,48 +9,43 @@ function GlobeView() {
   useEffect(() => {
     if (!globeRef.current) return;
 
-    const globe = Globe()(globeRef.current);
+    const timer = setTimeout(() => {
+      if (!globeRef.current) return;
 
-    // Earth texture
-    globe.globeImageUrl("//unpkg.com/three-globe/example/img/earth-dark.jpg");
-    globe.backgroundImageUrl("//unpkg.com/three-globe/example/img/night-sky.png");
-    globe.showAtmosphere(true);
-    globe.atmosphereColor("rgba(255, 255, 255, 0.6)");
-    globe.atmosphereAltitude(0.1);
-    
-    fetch("https://unpkg.com/world-atlas/countries-110m.json")
-      .then(res => res.json())
-      .then(worldData => {
-        const countries = topojson.feature(
-          worldData,
-          worldData.objects.countries
-        );
-        
-        const landMass = turf.combine(countries);
-        
-        const points = [];
-        const numPoints = 5000;
-        
-        for (let i = 0; i < numPoints; i++) {
-          const randomPoint = turf.randomPoint(1, { bbox: [-180, -90, 180, 90] });
-          const point = randomPoint.features[0];
-          
-          if (turf.booleanPointInPolygon(point, landMass.features[0])) {
-            const [lng, lat] = point.geometry.coordinates;
-            points.push({ lat, lng });
-          } else {
-            i--;
+      try {
+        const globe = new BarGlob3d(globeRef.current, airportsData, {
+          globeColor: "#050510",
+          globeOpacity: 1,
+          globeRadius: 150,
+          hexRes: 3,
+          hexPadding: 0.2,
+          barColor: "#00ffff",
+          barOpacity: 0.8,
+          barActiveColor: "#ffffff",
+          barActiveOpacity: 1,
+          highestBar: 0.15,
+          tooltipActiveBackgroundColor: "#0437F2",
+          tooltipActiveTextColor: "#ffffff",
+          tooltipValueSuffix: " passengers/yr",
+          tooltipsLimit: 10
+        });
+
+        return () => {
+          if (globe && typeof globe.dispose === 'function') {
+            globe.dispose();
           }
-        }
-        
-        globe
-          .pointsData(points)
-          .pointColor(() => "rgba(0, 180, 230, 0.8)")
-          .pointAltitude(0.01)
-          .pointRadius(0.1)
-          .pointsMerge(true);
-      });
+          if (globeRef.current) {
+            while (globeRef.current.firstChild) {
+              globeRef.current.removeChild(globeRef.current.firstChild);
+            }
+          }
+        };
+      } catch (error) {
+        console.error("Error creating globe:", error);
+      }
+    }, 100);
 
+    return () => clearTimeout(timer);
   }, []);
 
   return (
@@ -58,12 +53,10 @@ function GlobeView() {
       ref={globeRef}
       style={{ 
         width: "100%", 
-        height: "100%",
-        position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0
+        height: "100vh", 
+        position: "relative",
+        display: "block",
+        backgroundColor: "#000000"
       }}
     />
   );
